1a09a3ba9ea81a96435c38e4b8aaf05ebdec3d95
Ogabek <ogabekyusupov@gmail.com>
2018-10-25 00:35:52 +0500

    added splash activity

---
+++ app/src/main/java/startapp/SplashActivity.kt
@@ -0,0 +1,21 @@
+/*
+ * Created by Ogabek on 10/24/18 10:59 PM
+ */
+
+package startapp
+
+import android.content.Intent
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class SplashActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+
+}
