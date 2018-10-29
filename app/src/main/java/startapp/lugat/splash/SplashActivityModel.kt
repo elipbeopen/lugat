bc05ab5f5c1523afb77a08ac7a2f3818cb6e7e79
Ogabek <ogabekyusupov@gmail.com>
2018-10-29 21:52:52 +0500

    Fixed errors with splash screen

--- app/src/main/java/startapp/lugat/splash/SplashActivityModel.kt
+++ app/src/main/java/startapp/lugat/splash/SplashActivityModel.kt
@@ -2,7 +2,7 @@ package startapp.lugat.splash
 
 class SplashActivityModel: SplashActivityContract.MvpModel {
     override fun writeFromJsonToDb() {
-       TODO("We perform here writing to Sqlite database using room lib.")
+
     }
 
 }
